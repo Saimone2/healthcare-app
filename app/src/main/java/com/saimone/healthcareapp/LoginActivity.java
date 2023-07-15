db1e7fc85ae8ee7f75aa8fea44f4b90cc374e724
Andriy Khrushch <csgo.reserv@gmail.com>
2023-07-15 11:27:01 +0300

    listeners for registration and login pages buttons are implemented, field validation is added with throwing out a message about the incorrectness of the entered information

--- app/src/main/java/com/saimone/healthcareapp/LoginActivity.java
+++ app/src/main/java/com/saimone/healthcareapp/LoginActivity.java
@@ -2,13 +2,39 @@ package com.saimone.healthcareapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+    EditText etLoginUsername, etLoginPassword;
+    Button btnLogin;
+    TextView tvNewUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        etLoginUsername = findViewById(R.id.etLoginUsername);
+        etLoginPassword = findViewById(R.id.etLoginPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        tvNewUser = findViewById(R.id.tvNewUser);
+
+        btnLogin.setOnClickListener(view -> {
+            String username = etLoginUsername.getText().toString();
+            String password = etLoginPassword.getText().toString();
+
+            if(username.length() == 0 || password.length() == 0) {
+                Toast.makeText(getApplicationContext(), "Please enter all data", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getApplicationContext(), "Login Success", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        tvNewUser.setOnClickListener(view -> startActivity(new Intent(LoginActivity.this, RegisterActivity.class)));
     }
 }
