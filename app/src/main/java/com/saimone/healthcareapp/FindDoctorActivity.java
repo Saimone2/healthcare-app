a4e0a3f758ede49fe5712cd580a4bffc97baa3a4
Andriy Khrushch <csgo.reserv@gmail.com>
2023-07-17 13:31:56 +0300

    a page with a list of active doctors was created, test data about doctors was added

--- app/src/main/java/com/saimone/healthcareapp/FindDoctorActivity.java
+++ app/src/main/java/com/saimone/healthcareapp/FindDoctorActivity.java
@@ -13,6 +13,41 @@ public class FindDoctorActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_doctor);
 
+        CardView cvFDFamilyPhysician = findViewById(R.id.cvFDFamilyPhysician);
+        cvFDFamilyPhysician.setOnClickListener(view -> {
+            Intent it = new Intent(FindDoctorActivity.this, DoctorDetailsActivity.class);
+            it.putExtra("title", "Family Physician");
+            startActivity(it);
+        });
+
+        CardView cvFDDietician = findViewById(R.id.cvFDDietician);
+        cvFDDietician.setOnClickListener(view -> {
+            Intent it = new Intent(FindDoctorActivity.this, DoctorDetailsActivity.class);
+            it.putExtra("title", "Dietician");
+            startActivity(it);
+        });
+
+        CardView cvFDDentist = findViewById(R.id.cvFDDentist);
+        cvFDDentist.setOnClickListener(view -> {
+            Intent it = new Intent(FindDoctorActivity.this, DoctorDetailsActivity.class);
+            it.putExtra("title", "Dentist");
+            startActivity(it);
+        });
+
+        CardView cvFDSurgeon = findViewById(R.id.cvFDSurgeon);
+        cvFDSurgeon.setOnClickListener(view -> {
+            Intent it = new Intent(FindDoctorActivity.this, DoctorDetailsActivity.class);
+            it.putExtra("title", "Surgeon");
+            startActivity(it);
+        });
+
+        CardView cvFDCardiologist = findViewById(R.id.cvFDCardiologist);
+        cvFDCardiologist.setOnClickListener(view -> {
+            Intent it = new Intent(FindDoctorActivity.this, DoctorDetailsActivity.class);
+            it.putExtra("title", "Cardiologist");
+            startActivity(it);
+        });
+
         CardView cvFDBack = findViewById(R.id.cvFDBack);
         cvFDBack.setOnClickListener(view -> startActivity(new Intent(FindDoctorActivity.this, HomeActivity.class)));
     }
